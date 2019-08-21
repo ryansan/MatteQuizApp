ea069d5f17e72cbfec2df3c32859dcccc2702311
Ryan <ryan.sangha@hotmail.com>
2019-08-21 15:27:02 +0200

    new activity, read xml and started on checking if answer is correct

--- app/src/main/java/com/example/mattetest/MainActivity.java
+++ app/src/main/java/com/example/mattetest/MainActivity.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
@@ -13,6 +14,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Log.d("msg", "onCreate: hei ");
 
         final Button btnStart = findViewById(R.id.start);
 
--- app/src/main/java/com/example/mattetest/StartActivity.java
+++ app/src/main/java/com/example/mattetest/StartActivity.java
@@ -3,12 +3,57 @@ package com.example.mattetest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.Random;
 
 public class StartActivity extends AppCompatActivity {
+
+    public ArrayList<String> questions = new ArrayList<>();
+    public ArrayList<Integer> answers = new ArrayList<>();
+    public int preferedAmount = 4;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start);
+
+        getResFromXml();
+
+
+        final Button next = findViewById(R.id.next);
+
+        next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //test if answer is correct
+            }
+        });
+
+        TextView text = findViewById(R.id.txtQuestion);
+        text.setText(questions.get(0));
+    }
+
+    public void getResFromXml(){
+        String[] innQuestions = getResources().getStringArray(R.array.questions);
+        int[] innAnswers = getResources().getIntArray(R.array.answers);
+
+        Random random = new Random();
+
+        for(int i = 0; i < preferedAmount; i++){
+            //get random
+            int r = random.nextInt((9)+1);
+
+            //put random number from xml into arraylist
+            questions.add(innQuestions[r]);
+            answers.add(innAnswers[r]);
+
+            Log.d("q", questions.get(i));
+            Log.d("a", String.valueOf(answers.get(i)));
+        }
     }
 }
