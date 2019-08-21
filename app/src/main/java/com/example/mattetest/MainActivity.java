64ff3162bd07f9e9809089f3d5473657c3ca7c9d
Ryan <ryan.sangha@hotmail.com>
2019-08-21 12:02:39 +0200

    test

---
+++ app/src/main/java/com/example/mattetest/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.mattetest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        final Button btnStart = findViewById(R.id.start);
+
+        btnStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(MainActivity.this, StartActivity.class);
+                startActivity(intent);
+                //startActivity(new Intent(MainActivity.this,StartActivity.class));
+            }
+        });
+    }
+}
---
+++ app/src/main/java/com/example/mattetest/SecondActivity.java
@@ -0,0 +1,4 @@
+package com.example.mattetest;
+
+public class SecondActivity {
+}
