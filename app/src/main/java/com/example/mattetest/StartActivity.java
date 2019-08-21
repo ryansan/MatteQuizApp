d6a11a8d86a8f12451153c3d9bfb3c18244e9805
Ryan <ryan.sangha@hotmail.com>
2019-08-21 15:43:20 +0200

    started on checking if answer is correct + bugs

--- app/src/main/java/com/example/mattetest/StartActivity.java
+++ app/src/main/java/com/example/mattetest/StartActivity.java
@@ -6,7 +6,9 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -17,6 +19,9 @@ public class StartActivity extends AppCompatActivity {
     public ArrayList<Integer> answers = new ArrayList<>();
     public int preferedAmount = 4;
 
+    public EditText editText;
+    public TextView text;
+    public int counter = 0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,18 +29,36 @@ public class StartActivity extends AppCompatActivity {
 
         getResFromXml();
 
+        text = findViewById(R.id.txtQuestion);
+        text.setText(questions.get(counter));
 
         final Button next = findViewById(R.id.next);
 
         next.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+                editText = findViewById(R.id.temp);
+                if(Integer.valueOf(editText.getText().toString()) == answers.get(counter)){
+                    Log.d("A:", "Correct");
+                    Toast.makeText(StartActivity.this, "Correct", Toast.LENGTH_SHORT).show();
+                }else{
+                    Log.d("A:", "Wrong");
+                    Toast.makeText(StartActivity.this, "WRONG!", Toast.LENGTH_SHORT).show();
+                }
+
+                if(counter == preferedAmount){
+                    Toast.makeText(StartActivity.this, "Du er ferdig!", Toast.LENGTH_SHORT).show();
+                }
+
+                //increase counter and set next question
+                counter++;
+                text.setText(questions.get(counter));
+
                 //test if answer is correct
             }
         });
 
-        TextView text = findViewById(R.id.txtQuestion);
-        text.setText(questions.get(0));
+
     }
 
     public void getResFromXml(){
